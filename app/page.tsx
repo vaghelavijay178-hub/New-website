dd59dc44107c8d1e4b89d0f439c5dcfed8e9ad95
vaghelavijay178-hub <vaghelavijay178@gmail.com>
2026-03-29 17:22:21 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: "black",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "32px"
+    }}>
+      Vijay Website Live 🚀
+    </div>
+  );
+}
